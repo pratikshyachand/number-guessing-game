a98f428efda020dbf6332619c2caae0a59f82fc6
Pratikshya Chand <pratikshyachand07@gmail.com>
2026-02-02 11:21:52 +0545

    Initial commit

---
+++ main.py
@@ -0,0 +1,44 @@
+import random
+
+def user_guess(num):
+
+    chance = 5
+    guessed_num = []
+
+    while chance != 0:
+     try: 
+      print(f"Chances:{chance}")
+      guess = int(input("Guess the number (1-20): "))
+      
+      if guess in guessed_num:
+        print("You already used the number.")
+
+      else:
+         if guess == num:
+           print("You guessed it correctly!")
+           return True
+         elif guess < num :
+           print("Guessed number is less than the actual number")
+         elif guess > num:
+           print("Guessed number is greater than the actual one")
+
+         chance -= 1
+         guessed_num.append(guess)
+
+      if chance == 0:
+           print("You are out of guessing limits. Try again!")
+      
+     except ValueError:
+          print("Number must be an integer.")
+          pass
+
+def main():
+
+    print("*** NUMBER GUESSING GAME ***")
+    num = random.randint(1,20) # return random number within the range (1,100)
+    
+    user_guess(num)
+
+
+main()
+ 
